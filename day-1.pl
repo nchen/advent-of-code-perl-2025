63c667fd05372c50a8fdd0dae59a4a8ceee7617e
Neil Chen <chenrong2003@hotmail.com>
2025-12-13 22:29:55 +0800

    day 1 part 2

--- day-1.pl
+++ day-1.pl
@@ -38,4 +38,51 @@ sub part_1() {
     print "Zeros encountered: $zeros\n";
 }
 
+sub part_2() {
+    my $pos = 50;
+    my $zeros = 0;
+
+    foreach my $line (@lines) {
+        chomp($line);
+        
+        my $dir = substr($line, 0, 1);
+        my $steps = int(substr($line, 1));
+        $steps = int($steps);
+
+        my $passes = 0;
+        my $steps_to_zero = 0;
+        
+        if ($dir eq "R") {
+            if ($pos == 0) {
+                $steps_to_zero = 100;
+            } else {
+                $steps_to_zero = 100 - $pos;
+            }
+        } else { # L
+            if ($pos == 0) {
+                $steps_to_zero = 100;
+            } else {
+                $steps_to_zero = $pos;
+            }
+        }
+
+        if ($steps >= $steps_to_zero) {
+            $passes = int(($steps - $steps_to_zero) / 100) + 1;
+        }
+        
+        $zeros += $passes;
+
+        if ($dir eq "R") {
+            $pos = ($pos + $steps) % 100;
+        } else { # L
+            $pos = ($pos - $steps) % 100;
+        }
+
+        # normalize to 0..99, because Perl's % operator can yield negative results
+        $pos = ($pos % 100 + 100) % 100;
+    }
+    print "Zeros encountered: $zeros\n";
+}
+
 part_1();
+part_2();
